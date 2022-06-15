78079276629cffbb0aaae25409b39eecc05dd610
Gabriel Milanez <gabrielmilanez99@hotmail.com>
2022-06-15 16:56:43 -0300

    att

--- src/components/Covid.js
+++ src/components/Covid.js
@@ -9,6 +9,8 @@ const Covid = () => {
             const res = await fetch('https://api.covid19api.com/summary');
             const actualData = await res.json();
             console.log(actualData);
+            setNewdeaths(actualData.Global.NewDeaths);
+
             console.log(actualData.Global);
             console.log(actualData.Global.NewConfirmed);
             
