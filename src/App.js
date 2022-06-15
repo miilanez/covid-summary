9719bb7b80e6871d3c8526f73d67ba6061ca24d9
Gabriel Milanez <gabrielmilanez99@hotmail.com>
2022-06-15 20:53:11 -0300

    atualizacao dos arquivos

--- src/App.js
+++ src/App.js
@@ -1,18 +1,40 @@
+import React,{useState, useEffect} from "react";
 import './App.css';
 import Header from './components/Header';
 import Global from './components/Global';
 import Cardlist from './components/Cardlist';
 import Footer from './components/Footer';
-import Covid from './components/Covid';
 
 function App() {
+  const [globalData, setGlobalData] = useState({})
+  const [countriesData, setCountriesData] = useState([])
+
+  const getCovidData = async () => {
+      try {
+          const res = await fetch('https://api.covid19api.com/summary');
+          const actualData = await res.json();
+          console.log(actualData);
+          setGlobalData(actualData.Global);
+          setCountriesData(actualData.Countries);
+
+          console.log(actualData.Global);
+          console.log("==>", globalData);
+          
+      } catch (err) {
+          console.log(err);
+      }
+  }
+
+  useEffect(() => {
+      getCovidData()
+  }, []);
+
   return (
     <div className="App">
       <Header />
-      <Global />
-      <Cardlist />
+      <Global dados={globalData}/>
+      <Cardlist dados={countriesData}/>
       <Footer />
-      <Covid />
     </div>
   );
 }
--- src/components/Global.js
+++ src/components/Global.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-function Global() {
+function Global({dados}) {
     return (
         <div className="container-principal">
             <div className="container-global">
@@ -11,31 +11,33 @@ function Global() {
                 <div className="card-global">
                     <div className="card-global-esq">
                         <div className="principal-data">
-                            <h3>0</h3>
+                            <h3>{dados.TotalConfirmed.toLocaleString("pt-br")}</h3>
                             <p>Confirmed Cases</p>
                         </div>
 
                         <div className="principal-data">
-                            <h3>0</h3>
+                            <h3>{dados.TotalDeaths.toLocaleString("pt-br")}</h3>
                             <p>Cases of Death</p>
                         </div>
                     </div>
                     <hr />
                     <div className="card-global-center">
                         <div className="other-data">
-                            <h5> 0 </h5>
+                            <h5>
+                                {dados.NewDeaths.toLocaleString("pt-br")}
+                            </h5>
                             <p> New Deaths </p>
                         </div>
                         <div className="other-data">
-                            <h5> 0 </h5>
+                            <h5> {dados.NewConfirmed.toLocaleString("pt-br")} </h5>
                             <p> New Confirmeds </p>
                         </div>
                         <div className="other-data">
-                            <h5> 0 </h5>
+                            <h5> {dados.NewRecovered.toLocaleString("pt-br")} </h5>
                             <p> New Recovereds </p>
                         </div>
                         <div className="other-data">
-                            <h5> 0 </h5>
+                            <h5> {dados.TotalRecovered.toLocaleString("pt-br")} </h5>
                             <p> Total Recovereds </p>
                         </div>
                     </div>
