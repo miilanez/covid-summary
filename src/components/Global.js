c04677940ae298159f884e51330bb8ba0c77c09f
Gabriel Milanez <gabrielmilanez99@hotmail.com>
2022-06-17 19:31:20 -0300

    atualização dos cards

--- src/components/Global.js
+++ src/components/Global.js
@@ -5,40 +5,44 @@ function Global({dados}) {
         <div className="container-principal">
             <div className="container-global">
                 <div className="container-title">
-                    <p>#WASHYOURHANDS</p>
-                    <h1 className="card-title"> Global Situation </h1>
+                    <p>#LAVESUASMÃOS</p>
+                    <h1 className="card-title"> Situação Global </h1>
                 </div>
                 <div className="card-global">
                     <div className="card-global-esq">
                         <div className="principal-data">
-                            <h3>{dados.TotalConfirmed.toLocaleString("pt-br")}</h3>
-                            <p>Confirmed Cases</p>
+                        <h3 className="data-calendar"><span>Última Atualização: </span>{dados.Date}</h3>
+                            
+                        </div>
+                        <div className="principal-data">
+                            <h3>{dados.TotalConfirmed?.toLocaleString("pt-br")}</h3>
+                            <p>Casos Confirmados</p>
                         </div>
 
                         <div className="principal-data">
-                            <h3>{dados.TotalDeaths.toLocaleString("pt-br")}</h3>
-                            <p>Cases of Death</p>
+                            <h3>{dados.TotalDeaths?.toLocaleString("pt-br")}</h3>
+                            <p>Casos de Morte</p>
                         </div>
                     </div>
                     <hr />
                     <div className="card-global-center">
                         <div className="other-data">
                             <h5>
-                                {dados.NewDeaths.toLocaleString("pt-br")}
+                                {dados.NewDeaths?.toLocaleString("pt-br")}
                             </h5>
-                            <p> New Deaths </p>
+                            <p> Novas Mortes </p>
                         </div>
                         <div className="other-data">
-                            <h5> {dados.NewConfirmed.toLocaleString("pt-br")} </h5>
-                            <p> New Confirmeds </p>
+                            <h5> {dados.NewConfirmed?.toLocaleString("pt-br")} </h5>
+                            <p> Novas Confirmações </p>
                         </div>
                         <div className="other-data">
-                            <h5> {dados.NewRecovered.toLocaleString("pt-br")} </h5>
-                            <p> New Recovereds </p>
+                            <h5> {dados.NewRecovered?.toLocaleString("pt-br")} </h5>
+                            <p> Novos Recuperados </p>
                         </div>
                         <div className="other-data">
-                            <h5> {dados.TotalRecovered.toLocaleString("pt-br")} </h5>
-                            <p> Total Recovereds </p>
+                            <h5> {dados.TotalRecovered?.toLocaleString("pt-br")} </h5>
+                            <p> Total de Recuperados</p>
                         </div>
                     </div>
                     <div className="card-global-graphic">
--- src/components/Header.js
+++ src/components/Header.js
@@ -3,9 +3,9 @@ import React from "react";
 function Header() {
     return (
         <div className="header-topo">
-            <p><span>#</span>STAY<span>AT</span>HOME</p>
+            <p><span>#</span>FIQUE<span>EM</span>CASA</p>
             <h1><i>Tascom</i></h1>
-            <h2>Covid <span>Summary</span></h2>
+            <h2>Resumo <span>Covid19</span></h2>
         </div>
     );
 }
