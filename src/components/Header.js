74e049d84b079215fc4947b37cc070e9465b3312
Gabriel Milanez <gabrielmilanez99@hotmail.com>
2022-06-20 10:18:58 -0300

    mudancas

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,11 @@
 import React from "react";
+import image from "../assets/img/Slide-Back.png";
 
 function Header() {
     return (
-        <div className="header-topo">
+        <div className="header-topo" style={{ backgroundImage: `url(${image})`, backgroundRepeat: 'no-repeat', backgroundPosition: 'center', backgroundSize: 'cover'}}>
             <p><span>#</span>FIQUE<span>EM</span>CASA</p>
-            <h1><i>Tascom</i></h1>
+            <h1><i>Tascom - Gabriel Milanez</i></h1>
             <h2>Resumo <span>Covid19</span></h2>
         </div>
     );
